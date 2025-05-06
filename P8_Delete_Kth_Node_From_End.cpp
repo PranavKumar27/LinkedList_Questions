124ee30dfd49ea9be5fe9b8aa33c016de70388a4
Pranav Kumar <pranav.jenus@gmail.com>
2025-05-06 20:07:20 +0530

    Update P8_Delete_Kth_Node_From_End.cpp

--- P8_Delete_Kth_Node_From_End.cpp
+++ P8_Delete_Kth_Node_From_End.cpp
@@ -47,7 +47,7 @@ void Print_SLL(Node* head)
     cout << endl;
 }
 
-Node* deleteKthNode_fromEnd(Node* head,int k)
+Node* deleteKthNode_fromEnd_Sol1(Node* head,int k)
 {
     // Step1: Count the Nodes;
     int cnt = 0;
@@ -76,9 +76,11 @@ Node* deleteKthNode_fromEnd(Node* head,int k)
 
 
     temp = head;
-    while(temp!=nullptr && diff>1)
+    while(temp!=nullptr)
     {
         diff--;
+        if(diff==0)
+            break;
         temp = temp->next;
     }
 
@@ -90,18 +92,107 @@ Node* deleteKthNode_fromEnd(Node* head,int k)
     return head;
 
 }
+/*
+    Move fast Ptr k places k=2
+   fast
+    1    2     3      4     5
+
+        fast
+    1    2     3      4     5
+
+             fast
+    1    2     3      4     5
+
+    Now initial slow at head and move both fast and slow 1 step sequnetially till fast->next!=nullptr
+
+              fast
+    1    2     3      4     5
+   slow
+
+                     fast
+    1    2     3      4     5
+       slow
+
+                           fast
+    1    2     3      4     5              Here fast->next == null break;
+             slow
+
+
+    Now delete slow->next
+    Make connection as slow->next = slow->next->next
+    return head
+
+    if k=5 length of list
+
+    fast
+    1    2     3      4     5
+
+        fast
+    1    2     3      4     5
+
+             fast
+    1    2     3      4     5
+
+                    fast
+    1    2     3      4     5
+
+                          fast
+    1    2     3      4     5
+
+                                fast   Here fast=nullptr
+    1    2     3      4     5
+
+    if(fast==nullptr) it means remove first element or head
+    Hence newHead = head->next;
+          delete(head);
+          return newHead;
+
+*/
+
+Node* deleteKthNode_fromEnd_Sol2(Node* head,int k)
+{
+    Node* fast = head;
+    for(int i=0;i<k;i++)
+        fast  = fast->next;
+
+    if(fast==nullptr)
+    {
+        Node* delNode = head;
+        Node* neweHead = head->next;
+        free(delNode);
+        return neweHead;
+    }
+
+    Node* slow = head;
+    while(fast->next!=nullptr)
+    {
+        slow = slow->next;
+        fast = fast->next;
+    }
+
+    Node* delNode = slow->next;
+    slow->next = delNode->next;
+    free(delNode);
+    return head;
+}
 
 int main()
 {
     cout << "Delete Kth Node from End" << endl;
     vector<int> Arr = { 5,6,9,11,2,3,7};
-    int k = 7;
+    int k = 3;
     // Delete Kth Node from End
 
-    Node* head = convertArrToSLL(Arr);
-    Print_SLL(head);
-    head = deleteKthNode_fromEnd(head,k);
-    cout << "After Delete of Kth Node" << endl;
-    Print_SLL(head);
+    Node* head1 = convertArrToSLL(Arr);
+    Print_SLL(head1);
+    head1 = deleteKthNode_fromEnd_Sol1(head1,7);
+    cout << "Using Sol1 After Delete of Kth Node" << endl;
+    Print_SLL(head1);
+
+
+    Node* head2 = convertArrToSLL(Arr);
+    head2 = deleteKthNode_fromEnd_Sol2(head2,7);
+    cout << "Using Sol2 After Delete of Kth Node" << endl;
+    Print_SLL(head2);
     return 0;
 }
